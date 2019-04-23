39ad2111c21ceda273c536e6897b9b854acc3c8c
SerAlexei <alexseroshtanov2016@gmail.com>
2019-04-23 16:06:57 +0300

    Change validator form

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import firebase from '../Firebase';
 import {Link} from 'react-router-dom';
 import {Button} from "semantic-ui-react";
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-
 
 class Create extends Component{
     constructor(){
@@ -17,7 +15,7 @@ class Create extends Component{
             email: '',
             company:  '',
             image: null,
-            url: ''
+            url: '',
         };
     }
     handleChange = (e) =>{ //Добавление изображения в firebase storage, получение url изображения и запись url в firebase database
@@ -40,6 +38,7 @@ class Create extends Component{
         const state = this.state;
         state[e.target.name] = e.target.value;
         this.setState(state);
+
     };
     onSubmit = (e) => {  //Получение данных полей формы и запись в firebase database
         e.preventDefault();
@@ -68,54 +67,53 @@ class Create extends Component{
         });
     };
 
-
     render() {
         const {phone_number, lastName, firstName, email, company, url} = this.state;
+
         return (
+
             <div className='ui container'>
                 <div className="ui segment">
                 <h3 className='ui header'>  Add Number </h3>
                 <Button><Link to='/'> Back To Phone List</Link></Button>
-                <ValidatorForm className="ui form ui segment" onSubmit={this.onSubmit}>
-                    <div className="field ">
+                <div className="ui form ui segment" onSubmit={this.onSubmit}>
+                    <form className="ui form">
+                    <div className="field" >
                         <label>First Name:</label>
-                        <TextValidator
-                            validators={['required']}
-                            errorMessages={['This field is required', 'First name is not valid']}
+                        <input
+                            required
                             type="text" name="firstName" placeholder="First Name" value={firstName} onChange={this.onChange}/>
                     </div>
-                    <div className="field">
+                    <div className="field" >
                         <label>Last Name</label>
-                        <TextValidator
-                            validators={['required']}
-                            errorMessages={['This field is required', 'Last name is not valid']}
+                        <input
+                            required
                             type="text" name="lastName" placeholder="Last Name" value={lastName} onChange={this.onChange}/>
                     </div>
-                    <div className="field">
+                    <div className="field" >
                         <label>Phone Number</label>
-                        <TextValidator
-                            validators={['required']}
-                            pattern={'/^\\(?([0-9]{3})\\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/'}
-                            errorMessages={['This field is required', 'Phone number is not valid']}
-                            type="text" name="phone_number" placeholder="Phone Number" value={phone_number} onChange={this.onChange}/>
+                        <input
+                            required
+                            type="number"
+                            name="phone_number" placeholder="Phone Number" value={phone_number} onChange={this.onChange}/>
                     </div>
-                    <div className="field">
+                        <div className="field" >
                         <label>E-mail</label>
-                        <TextValidator
-                            validators={['required', 'isEmail']}
-                            errorMessages={['This field is required', 'Email is not valid']}
-                             name="email" placeholder="E-mail" value={email} onChange={this.onChange}/>
-                    </div>
-                    <div className="field">
+                        <input
+                            required type="email" name="email" placeholder="E-mail" value={email} onChange={this.onChange}/>
+                    </div >
+                    <div className="field" >
                         <label>Company</label>
-                        <TextValidator
-                            validators={['required']}
-                            errorMessages={['This field is required', 'Company is not valid']}
+                        <input
+                            required
                             type="text" name="company" placeholder="Company" value={company} onChange={this.onChange}/>
-                    </div>
+                    </div >
+                        <div className="field" >
                     <input required type="file" className="fileInput" data-url={url} onChange={this.handleChange}  />
+                        </div>
                     <button className="ui button ui segment" type="submit">Add number</button>
-                </ValidatorForm>
+                </form>
+            </div>
             </div>
             </div>
         );
